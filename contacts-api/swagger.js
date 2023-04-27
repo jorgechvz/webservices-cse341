cf40c1f72bac5edc2f2674001d225ac09e75bb4b
jorgechvz <cha22060@byui.edu>
2023-04-27 10:39:29 -0500

    Add Swagger to Project

---
+++ contacts-api/swagger.js
@@ -0,0 +1,19 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Contacts API',
+  },
+  host: 'https://personal-assignmentw01.onrender.com',
+  schemes: ['http'],
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/routes.js'];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
