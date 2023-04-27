0479c2d5725644e364e840ede615e84e1979456e
jorgechvz <cha22060@byui.edu>
2023-04-27 11:58:36 -0500

    Fixed

--- contacts-api/swagger.js
+++ contacts-api/swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'Contacts API',
   },
   host: 'https://personal-assignmentw01.onrender.com',
-  schemes: ['http'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
