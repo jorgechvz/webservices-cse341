7f78c7ad3ddf157df621f4fea5539e8910ef952c
jorgechvz <cha22060@byui.edu>
2023-04-28 14:53:59 -0500

    Fixed

--- contacts-api/app.js
+++ contacts-api/app.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app
-  .use('/', require('./src/routes'))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -19,6 +18,7 @@ app
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
+  .use('/', require('./src/routes'))
   .use((req, res) => {
     res.status(400).send('Sorry cant find that');
   })
